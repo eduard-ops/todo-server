791b78884d559e0dbd6b6c2a245615d1224b4ab7
eduard-ops <epustynnik@gmail.com>
2022-11-20 18:08:11 +0200

    aaa

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const app = require("./app");
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+async function bootstrap() {
+  try {
+    await app.listen(PORT);
+    console.log("let's go");
+  } catch (error) {
+    console.log(error, "disconnect");
+  }
+}
+bootstrap();
