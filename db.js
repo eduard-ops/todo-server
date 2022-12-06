22d68d5cb97d51332f49b46c0e6cea1f1242ede8
eduard-ops <epustynnik@gmail.com>
2022-12-06 09:35:49 +0200

    add

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
